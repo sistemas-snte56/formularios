6db78762a526e6c9e81bb7a239c4c4f47c5a74fc
sistemas-snte56 <computo.seccional@outlook.com>
2024-09-11 11:58:08 -0600

    Instalación y Personalización completa

--- app/Models/User.php
+++ app/Models/User.php
@@ -58,4 +58,14 @@ class User extends Authenticatable
     protected $appends = [
         'profile_photo_url',
     ];
+
+    public function adminlte_image()
+    {
+        return url($this->profile_photo_url);
+    }
+    
+    public function adminlte_profile_url()
+    {
+        return url('user/profile');
+    }
 }
--- config/adminlte.php
+++ config/adminlte.php
@@ -136,9 +136,9 @@ return [
     'usermenu_enabled' => true,
     'usermenu_header' => false,
     'usermenu_header_class' => 'bg-primary',
-    'usermenu_image' => false,
-    'usermenu_desc' => false,
-    'usermenu_profile_url' => false,
+    'usermenu_image' => true,
+    'usermenu_desc' => true,
+    'usermenu_profile_url' => true,
 
     /*
     |--------------------------------------------------------------------------
